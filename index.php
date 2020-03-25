8103bb0abd77caf6a27335ba8dc8d910d427df3d
JulienGrade <gradejulien@gmail.com>
2020-03-25 16:39:19 +0100

    Add dependency injection and use definition in container

--- index.php
+++ index.php
@@ -4,13 +4,42 @@ use App\Controller\OrderController;
 use App\Database\Database;
 use App\Mailer\GmailMailer;
 use App\Texter\SmsTexter;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 
 require __DIR__ . '/vendor/autoload.php';
 
-$database = new Database();
-$texter = new SmsTexter("service.sms.com", "apikey123");
-$mailer = new GmailMailer("lior@gmail.com", "123456");
-$controller = new OrderController($database, $mailer, $texter);
+$container = new ContainerBuilder();
+
+$databaseDefinition = new Definition(Database::class);
+
+$container->setDefinition('database', $databaseDefinition);
+//$container->set('database', new Database());
+
+$smsTexterDefinition = new Definition(SmsTexter::class);
+/*$smsTexterDefinition->addArgument("service.sms.com")
+                    ->addArgument("apikey123");*/
+$smsTexterDefinition->setArguments([
+    "service.sms.com",
+    "apikey123"
+]);
+$container->setDefinition('texter.sms', $smsTexterDefinition);
+
+$gmailMailerDefinition = new Definition(GmailMailer::class, [
+   'lior@gmail.com',
+   '123456'
+]);
+$container->setDefinition('mailer.gmail', $gmailMailerDefinition);
+
+$controllerDefinition = new Definition(OrderController::class, [
+    $container->get('database'),
+    $container->get('mailer.gmail'),
+    $container->get('texter.sms')
+]);
+$container->setDefinition('order_controller', $controllerDefinition);
+
+$controller = $container->get('order_controller');
+//$controller = new OrderController($database, $mailer, $texter);
 
 $httpMethod = $_SERVER['REQUEST_METHOD'];
 
